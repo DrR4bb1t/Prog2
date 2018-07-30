54b9a28798acb5523e91a53390c4433b8e6d1652
Dr. Rabbit <huber.andreas123@gmail.com>
2018-07-30 17:28:00 +0200

    Animation-Classes added

---
+++ Project_OD/Managers/Animation.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Content;
+
+namespace Project_OD.Managers
+{
+    public class Animation
+    {
+        protected Texture2D img;
+        protected string text;
+        protected SpriteFont font;
+        protected Color color;
+        protected Rectangle srcRect;
+        protected float rotation, scale;
+        protected Vector2 origin, position;
+
+
+
+    }
+}
