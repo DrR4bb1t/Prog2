089598b724e47f709cce5790b6abcc49523a73fa
FelixS <felix.s96ss@gmail.com>
2018-09-03 13:21:10 +0200

    player bewegt sich statt sprite

--- Project_OD/Entities/Entity.cs
+++ Project_OD/Entities/Entity.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Content;
 using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -14,54 +15,56 @@ namespace Project_OD
     {
         public Entity() { }
         //Create Entity
-        public void SetEntity(Vector2 position, int width, int height, Texture2D texture, Texture2D deathTexture, float maxSpeed, float acceleration, float jumpHeight, int maxHp, int baseAtk,float atkRange)
+        public void SetEntity(Vector2 position, int width, int height, Texture2D texture, Texture2D deathTexture, float maxSpeed, float jumpMaxSpeed, int maxHp, int baseAtk,float atkRange, float atkTimeout)
         {
             this.position = position;
             this.width = width;
             this.height = height;
             this.texture = texture;
             this.deathTexture = deathTexture;
-            this.maxSpeed = maxSpeed;
-            this.acceleration = acceleration;
-            this.jumpHeight = jumpHeight;
+            this.jumpMaxSpeed = jumpMaxSpeed;
             this.maxHp = maxHp;
             this.baseAtk = baseAtk;
             this.atkRange = atkRange;
+            this.atkTimeout = atkTimeout;
         }
-        //public void Update()
-        //{
+        public void Update()
+        {
+            
+           
+            //Animated Sprite
 
-        //    //Animated Sprite
-
-        //}
+        }
         public void Destroy() { }
         //Private Properties
-        protected Vector2 position;
-        protected int width;
-        protected int height;
+        protected Vector2 moveTo;
+        //Physics physics = new Physics();    //create generally
+        protected Vector2 position=new Vector2(0, 850);
+        protected int width=1;
+        protected int height=1;
         protected Texture2D texture;
         protected Texture2D deathTexture;
-        protected float maxSpeed;
-        protected float speed;
-        protected float acceleration;
-        protected float jumpHeight;
-        protected float jumpSpeed;
+        protected float speed=200;
+        protected float jumpMaxSpeed=1;
+        protected float jumpSpeed=0;
         protected int maxHp;
         protected int hp;
         protected int baseAtk;
         protected int atk;
         protected float atkRange;
-        
+        protected float atkTimeout;
+
         //?Add Frame for Animated?
 
         //Modifiers required to be changeable
         public Vector2 Position { get => position; set => position = value; }
-        public float Speed { get => speed; set { if (value <= maxSpeed) speed = value; } }
+        public float Speed { get => speed; set => speed = value; } 
         //Add Acceleration if slows
         public float JumpSpeed { get => jumpSpeed; set => jumpSpeed = value; }
         public int Hp { get => hp; set => hp = value; }
         public int Atk { get => atk; set => atk = value; }
         public float AtkRange { get => atkRange; set => atkRange = value; }
+        public float GetjumpMaxSpeed() { return jumpMaxSpeed; }
 
     }
 }
--- Project_OD/Entities/Player.cs
+++ Project_OD/Entities/Player.cs
@@ -15,8 +15,9 @@ namespace Project_OD
         //public Player() { }
         private int armorValue;
         private int weaponValue;
+        private string dir;
+        Physics physics = new Physics();
 
-        
         public int ArmorValue { get => armorValue; set => armorValue = value; }
         public int WeaponValue { get => weaponValue; set => weaponValue = value; }
 
@@ -46,21 +47,43 @@ namespace Project_OD
         public void Update(GameTime gameTime, int fps)
         {
             KeyboardState state = Keyboard.GetState();
-
-            if (state.IsKeyDown(Keys.Right))
-            {
-                sprite.animation = "R";
-                sprite.position.X += (float)(gameTime.ElapsedGameTime.TotalSeconds * 200);
-                sprite.Update(gameTime, true, fps);
+            dir = "";
+            if (state.IsKeyDown(Keys.Right)) {
+                dir = "R";
             }
             else if (state.IsKeyDown(Keys.Left))
             {
-                sprite.animation = "L";
-                sprite.position.X -= (float)(gameTime.ElapsedGameTime.TotalSeconds * 200);
+                dir = "L";
+            }
+            moveTo = physics.Update(this, gameTime,dir);
+            Position += moveTo;
+            if (moveTo.X > 0)
+            {
+                sprite.animation = "R";
                 sprite.Update(gameTime, true, fps);
             }
+            else if (moveTo.X < 0)
+            {
+                sprite.animation = "L";
+                sprite.Update(gameTime, true, fps);
+            }
+            sprite.position.X = Position.X;
+            sprite.position.Y = Position.Y;
             
+            //if (state.IsKeyDown(Keys.Right))
+            //{
+            //    sprite.animation = "R";
+            //    sprite.position.X += (float)(gameTime.ElapsedGameTime.TotalSeconds * 200);//sprite.position=player.position
+            //    sprite.Update(gameTime, true, fps);
+            //}
+            //else if (state.IsKeyDown(Keys.Left))
+            //{
+            //    sprite.animation = "L";
+            //    sprite.position.X -= (float)(gameTime.ElapsedGameTime.TotalSeconds * 200);
+            //    sprite.Update(gameTime, true, fps);
+            //}
 
+            Console.WriteLine("X: {0}, Y: {1}", position.X, position.Y);
         }
 
         public void Draw(SpriteBatch spriteBatch)
