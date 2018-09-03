671e92ca773103257f06af4385fc2a4334824d89
dannyqq <38943889+dannyqq@users.noreply.github.com>
2018-09-03 14:59:50 +0200

    tutorial for keyboard added.

--- Project_OD/Managers/InputManager.cs
+++ Project_OD/Managers/InputManager.cs
@@ -11,30 +11,27 @@ namespace Project_OD
     class InputManager
     {
         /// <summary>
-        /// this class handles the mouse and keyboard input
+        /// Tutorial Keyboard Manager:
+        /// Snippet -
+        /// if(InputManager.IsKeyPressed(Keys.Enter) {do Something }
+        /// call the Input Managers 'isKeyPressed' Method and pass 'Keys.YourKey'
         /// </summary>
-        
         private static KeyboardState currentKeyboardState;
         private static KeyboardState lastKeyboardState;
 
-        //this function holds the current keyboard state
         public static KeyboardState CurrentKeyboardState
         {
-            //this statement gets and returns the current keyboard state
             get { return currentKeyboardState; }
         }
 
-        //this function tells if a key is being pressed or not
         public static bool IsKeyPressed(Keys key)
         {
-            //this statement returns which key is being pressed
+
             return currentKeyboardState.IsKeyDown(key);
         }
 
-        //this function tells if a key is being pressed in the currentState
         public static bool IsKeyTriggered(Keys key)
         {
-            //returns which key is currently being pressed
             return (currentKeyboardState.IsKeyDown(key)) &&
                 (!lastKeyboardState.IsKeyDown(key));
         }
@@ -42,34 +39,24 @@ namespace Project_OD
         private static MouseState currentMouseState;
         private static MouseState lastMouseState;
 
-        //this function  holds the mouse boundaries inside a rectanglle
         public static Rectangle GetMouseBoundaries(bool currentState)
         {
             if (currentState)
-                //returns a Rectangle containing the current Mouse Position
                 return new Rectangle(currentMouseState.X, currentMouseState.Y, 1, 1);
-            //returns a Rectangle containing the last Mouse Position
             else
                 return new Rectangle(lastMouseState.X, lastMouseState.Y, 1, 1);
         } 
 
-        //this function checks if there is no mouse button up
         public static bool GetIsMouseButtonUp(MouseButton button, bool currentState)
         {
             if (currentState)
                 switch (button)
                 {
                     case MouseButton.LeftButton:
-                        //returns that the left mouse button is being released in the current
-                        //mouse state
                         return currentMouseState.LeftButton == ButtonState.Released;
                     case MouseButton.MiddleButton:
-                        //returns that the middle mouse button is being released in the current
-                        //mouse state
                         return currentMouseState.MiddleButton == ButtonState.Released;
                     case MouseButton.RightButton:
-                        //returns that the right mouse button is being released in the current
-                        //mouse state
                         return currentMouseState.RightButton == ButtonState.Released;
                 }
             else
@@ -86,10 +73,8 @@ namespace Project_OD
             return false;
         }
 
-        //this function holds if there is a mouse button down
         public static bool GetIsMouseButtonDown(MouseButton button, bool currentState)
         {
-            //returns that the specific mouse button is being pressed
             return !GetIsMouseButtonUp(button, currentState);
         }
 
@@ -100,7 +85,6 @@ namespace Project_OD
             RightButton
         }
 
-        //this function updates the state of mouse and keyboard
         public static void Update()
         {
             lastKeyboardState = currentKeyboardState;
