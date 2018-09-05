1a0a454c3c8e6d354da6dd3ce59063c915b28a66
FelixS <felix.s96ss@gmail.com>
2018-09-05 13:36:43 +0200

    removed sprites in player

--- Project_OD/Entities/Player.cs
+++ Project_OD/Entities/Player.cs
@@ -22,29 +22,8 @@ namespace Project_OD
         public int WeaponValue { get => weaponValue; set => weaponValue = value; }
 
         #region test for playeranimation
-
-        //SpriteAnimation sprite;
-
-
-        //public void LoadTexture()
-        //{
-        //    texture = OD.content.Load<Texture2D>("spritesheet-test2_1.png");
-        //}
         
-
-
-        //public Player(int coordX, int coordY, int frames, int animations)
-        //{
-        //    LoadTexture();
-
-        //    sprite = new SpriteAnimation(texture, new Vector2(coordX, coordY), "R", frames, animations);
-        //    sprite.StoreAnimations();
-            
-            
-        //}
-
-
-        public void Update(GameTime gameTime, int fps)
+        public new void Update(GameTime gameTime, int fps)
         {
             KeyboardState state = Keyboard.GetState();
             dir = "";
@@ -58,41 +37,9 @@ namespace Project_OD
             moveTo = physics.Update(this, gameTime,dir);
             Position += moveTo;
             spriteanim(gameTime, fps);
-            //if (moveTo.X > 0)
-            //{
-            //    sprite.animation = "R";
-            //    sprite.Update(gameTime, true, fps);
-            //}
-            //else if (moveTo.X < 0)
-            //{
-            //    sprite.animation = "L";
-            //    sprite.Update(gameTime, true, fps);
-            //}
-            //sprite.position.X = Position.X;
-            //sprite.position.Y = Position.Y;
-            
-            //if (state.IsKeyDown(Keys.Right))
-            //{
-            //    sprite.animation = "R";
-            //    sprite.position.X += (float)(gameTime.ElapsedGameTime.TotalSeconds * 200);//sprite.position=player.position
-            //    sprite.Update(gameTime, true, fps);
-            //}
-            //else if (state.IsKeyDown(Keys.Left))
-            //{
-            //    sprite.animation = "L";
-            //    sprite.position.X -= (float)(gameTime.ElapsedGameTime.TotalSeconds * 200);
-            //    sprite.Update(gameTime, true, fps);
-            //}
-
+           
             Console.WriteLine("X: {0}, Y: {1}", position.X, position.Y);
         }
-
-        //public void Draw(SpriteBatch spriteBatch)
-        //{
-
-        //    sprite.Draw(spriteBatch);
-
-        //}
 
            #endregion
     }
