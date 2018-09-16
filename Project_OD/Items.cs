40a1a145605076dfa895ffe722d8c085f0923ae5
dannyqq <38943889+dannyqq@users.noreply.github.com>
2018-09-16 11:14:08 +0200

    update logic missing.
    //todo list
    //update logic

--- Project_OD/Items.cs
+++ Project_OD/Items.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,42 @@ using System.Threading.Tasks;
 
 namespace Project_OD
 {
-    class Items
+    public class Items
     {
+        private Texture2D item;
+
+        private int itemDMGValue;
+        private int itemArmourValue;
+
+        private bool inIventar = false;
+
+        public int ItemDMGValue { get; set; }
+        public int ItemArmourValue { get; set; }
+
+        public Items(int itemID, Vector2 pos)
+        {
+            //itemid
+            ItemDMGValue = itemDMGValue;
+            ItemArmourValue = itemArmourValue;
+            ItemDMGValue = itemDMGValue;
+            ItemArmourValue = itemArmourValue;
+
+            Rectangle itemRect = new Rectangle((int)pos.X, (int)pos.Y, item.Width, item.Height);
+        }
+
+        public void Update()
+        {
+           //if item rect intersect with player position and button is pressed
+           //in inventar is true
+
+            //if in inventar and button is pressed
+            // is equipped is true
+            //and values get added to the players current stats
+        }
+
+        public void Draw()
+        {
+
+        }
     }
 }
